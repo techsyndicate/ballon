abd6f3fe77d2b00626931adfbf7867f42fd39c47
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-08-28 20:28:15 +0530

    inflate

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const db = require('../config/db');
 const User = db.collection('users');
+const Election = db.collection('elections');
+const Party = db.collection('parties');
 const auth = require('../lib/auth');
+const indianCitiesDatabase = require('indian-cities-database');
+var username;
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
@@ -19,6 +23,7 @@ router.post('/login', (req, res, next) => {
     successRedirect: '/dashboard',
     failureRedirect: '/login'
   })(req, res, next);
+  username = req.body.username;
 });
 
 // router.get('/register', (req, res) => {
@@ -35,7 +40,38 @@ router.get('/logout', (req, res) => {
 });
 
 router.get('/dashboard', auth.checkAuth, (req, res) => {
-  res.render('dashboard');
+  var cities = indianCitiesDatabase.cities;
+  var state;
+  var elections = {};
+  User.doc(username).get()
+  .then(user => {
+    var city = user.data()['address'].split(',')[user.data()['address'].split(',').length - 1].trim()
+    cities.forEach((value, index, array) => { 
+      if (value['city'] == city) {
+        state = value['state']
+      }
+    })
+  })
+  Election.where('ongoing', '==', true).get()
+  .then(electionSnapshot => {
+    electionSnapshot.forEach(election => {
+      elections[election.id] = election.data();
+    })
+    for (var key in elections) {
+      if (elections[key]['state'] == state || elections[key]['area'] == 'India') {
+        elections[key]['active'] = true;
+      } else {
+        elections[key]['active'] = false;
+      }
+    }
+    console.log(state);
+    console.log(elections);
+    res.render('dashboard', {elections: elections});
+  })
+})
+
+router.post('/election', auth.checkAuth, (req, res) => {
+  
 })
 
 module.exports = router;
