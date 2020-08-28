b79410c91119a5e4512f99cb562b1ef401e9b2d5
Laxya Pahuja <laxya.pahuja8@gmail.com>
2020-08-29 04:22:11 +0530

    laxya the codingpr0

--- routes/index.js
+++ routes/index.js
@@ -36,25 +36,25 @@ router.get('/dashboard', auth.checkAuth, (req, res) => {
   const cities = indianCitiesDatabase.cities;
   let state;
   let elections = {};
+  let vote = {};
   User.doc(username).get()
   .then(user => {
     const city = user.data()['address'].split(',')[user.data()['address'].split(',').length - 1].trim()
+    vote = user.data()['vote']
     cities.forEach((value, index, array) => { 
       if (value['city'] == city) {
         state = value['state'];
       }
     });
   });
-
   Election.where('ongoing', '==', true).get()
   .then(electionSnapshot => {
     electionSnapshot.forEach(election => {
       elections[election.id] = election.data();
     })
     for (var key in elections) {
-      if (elections[key]['state'] == state || elections[key]['area'] == 'India') {
+      if (elections[key]['state'] == state || elections[key]['area'] == 'India' && !(elections[key]['id'] in vote)) {
         elections[key]['active'] = true;
-        console.log(elections[key]['date']['_seconds'])
         var date = new Date(1000 * elections[key]['date'])
         elections[key]['date'] = date.toString('').split(' ')[2] + ' ' + date.toString('').split(' ')[1]
       } else {
@@ -88,16 +88,23 @@ router.get('/election', (req, res) => {
       partySnapshot.forEach(party => {
         parties.push(party.data());
       });
-      res.render('election', { parties: parties, title: `${electionId} Elections`, checkVote: checkVote, votedFor: votedFor });
+      res.render('election', { parties: parties, title: `${electionId} Elections`, checkVote: checkVote, votedFor: votedFor, id: electionId });
     }).catch(err => console.log(err));
 });
 
 router.post('/election', (req, res) => {
-  const name = req.body.name;
-  User.doc(username).update({
-    votedFor: name,
-    voted: true
-  });
-  console.log(`You've successfully voted for ${name}.`);
+  const party = req.body.party;
+  const electionId = req.body.electionId;
+  console.log(electionId)
+  var vote = {};
+  User.doc(username).get()
+  .then(user => {
+    vote = user.data()['vote']
+    vote[electionId] = party
+    User.doc(username).update({
+      vote: vote
+    });
+    res.render('dashboard')
+  })
 });
 module.exports = router;
